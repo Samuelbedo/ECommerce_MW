6bdb61d3edf4b2c8cf89e7294bc66d43edbb56b9
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2023-03-15 12:24:03 -0500

    New categories table created

--- ECommerce_MW/DAL/DataBaseContext.cs
+++ ECommerce_MW/DAL/DataBaseContext.cs
@@ -12,10 +12,13 @@ namespace ECommerce_MW.DAL
 
         public DbSet<Country> Countries { get; set; }
 
+        public DbSet<Category> Categories { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();//define que no pueden haber dos countries con el mismo nombre
+            modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
         }
     }
 }
