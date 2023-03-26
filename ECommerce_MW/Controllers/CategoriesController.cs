1a1bb0cb55675fdfd9c28415881130b7905c8a29
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2023-03-25 23:32:16 -0500

    Index action and view created for categories

---
+++ ECommerce_MW/Controllers/CategoriesController.cs
@@ -0,0 +1,40 @@
+﻿using ECommerce_MW.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce_MW.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly DataBaseContext _context; //no debe estar inicializada 
+
+        public CategoriesController(DataBaseContext context)//inyeccion de dependencia 
+        {
+            _context = context;
+        }
+
+        // GET: Categories
+        public async Task<IActionResult> Index()//se debe crear la vista del index *click derecho addview
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+        // GET: Categories/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var category= await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);//Sirve para traer un dato y coincidirlo con la tabla, trae el primero que encuentre 
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+    }
+}
